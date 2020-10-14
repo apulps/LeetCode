5441cc9b9e21897fa98f7d6bf59e3e731d8b7480
apulps <apinedaminon@gmail.com>
2020-10-15 00:48:18 +0200

    Move tests

--- tests.py
+++ tests.py
@@ -1,18 +1,59 @@
 import unittest
-from two_sum import two_sum, two_sum_2
-from reverse_integer import reverse_integer
-from running_sum import running_sum, running_sum_2, running_sum_3
-from kids_with_candies import kids_with_candies, kids_with_candies_2
-from shuffle import shuffle
-from num_identical_pairs import num_identical_pairs
-from defang_IP_addr import defang_IP_addr
-from num_jewels_in_stones import num_jewels_in_stones, num_jewels_in_stones_2
-from number_of_steps import number_of_steps
-from shuffle_array import shuffle_array
-from smaller_numbers_than_current import smaller_numbers_than_current
-from subtract_product_and_sum import subtract_product_and_sum
-from decompress_RLE_list import decompress_RLE_list
-from max_depth import max_depth
+from array_problems.remove_duplicates import remove_duplicates, remove_duplicates_2
+
+from easy_problems.two_sum import two_sum, two_sum_2
+from easy_problems.reverse_integer import reverse_integer
+from easy_problems.running_sum import running_sum, running_sum_2, running_sum_3
+from easy_problems.kids_with_candies import kids_with_candies, kids_with_candies_2
+from easy_problems.shuffle import shuffle
+from easy_problems.num_identical_pairs import num_identical_pairs
+from easy_problems.defang_IP_addr import defang_IP_addr
+from easy_problems.num_jewels_in_stones import num_jewels_in_stones, num_jewels_in_stones_2
+from easy_problems.number_of_steps import number_of_steps
+from easy_problems.shuffle_array import shuffle_array
+from easy_problems.smaller_numbers_than_current import smaller_numbers_than_current
+from easy_problems.subtract_product_and_sum import subtract_product_and_sum
+from easy_problems.decompress_RLE_list import decompress_RLE_list
+from easy_problems.max_depth import max_depth
+
+from medium_problems.subrectangle_queries import SubrectangleQueries
+from medium_problems.group_the_people import group_the_people
+from medium_problems.max_increase_keeping_skyline import max_increase_keeping_skyline
+
+
+class TestArrayProblems(unittest.TestCase):
+    def test_remove_duplicates(self):
+        nums = [1,1,2]
+        result = remove_duplicates(nums)
+        self.assertEqual(result, 2)
+        self.assertEqual(nums, [1,2])
+
+        nums = [0,0,1,1,1,2,2,3,3,4]
+        result = remove_duplicates(nums)
+        self.assertEqual(result, 5)
+        self.assertEqual(nums, [0,1,2,3,4])
+
+        nums = [1,1]
+        result = remove_duplicates(nums)
+        self.assertEqual(result, 1)
+        self.assertEqual(nums, [1])
+    
+    def test_remove_duplicates_2(self):
+        nums = [1,1,2]
+        result = remove_duplicates_2(nums)
+        self.assertEqual(result, 2)
+        self.assertEqual(nums, [1,2])
+
+        nums = [0,0,1,1,1,2,2,3,3,4]
+        result = remove_duplicates_2(nums)
+        self.assertEqual(result, 5)
+        self.assertEqual(nums, [0,1,2,3,4])
+
+        nums = [1,1]
+        result = remove_duplicates_2(nums)
+        self.assertEqual(result, 1)
+        self.assertEqual(nums, [1])
+
 
 
 class TestEasyProblems(unittest.TestCase):
@@ -294,6 +335,98 @@ class TestEasyProblems(unittest.TestCase):
         s = "1"
         result = max_depth(s)
         self.assertEqual(result, 0)
+
+
+
+class TestMediumProblems(unittest.TestCase):
+    def test_subrectangle_queries(self):
+        subrectangle_queries = SubrectangleQueries([[1,2,1],[4,3,4],[3,2,1],[1,1,1]])
+        result = subrectangle_queries.get_value(2,2)
+        self.assertEqual(result, 1)
+
+        result = subrectangle_queries.get_value(1,0)
+        self.assertEqual(result, 4)
+
+        result = subrectangle_queries.get_value(3,1)
+        self.assertEqual(result, 1)
+
+        subrectangle_queries.update_subrectangle(0,0,1,2,100)
+        result = subrectangle_queries.get_value(0,1)
+        self.assertEqual(result, 100)
+
+        subrectangle_queries.update_subrectangle(2,0,2,2,90)
+        result = subrectangle_queries.get_value(2,1)
+        self.assertEqual(result, 90)
+
+        subrectangle_queries.update_subrectangle(3,0,3,2,80)
+        result = subrectangle_queries.get_value(3,1)
+        self.assertEqual(result, 80)
+
+        subrectangle_queries = SubrectangleQueries([[6,9,6,1,2],[8,8,6,5,9],[7,6,10,8,2],[7,7,4,9,1]])
+        subrectangle_queries.update_subrectangle(1,4,2,4,5)
+        result = subrectangle_queries.get_value(3,4)
+        self.assertEqual(result, 1)
+
+        subrectangle_queries.update_subrectangle(3,4,3,4,8)
+        result = subrectangle_queries.get_value(2,0)
+        self.assertEqual(result, 7)
+
+    def test_subrectangle_queries_2(self):
+        subrectangle_queries = SubrectangleQueries([[1,2,1],[4,3,4],[3,2,1],[1,1,1]])
+        result = subrectangle_queries.get_value(2,2)
+        self.assertEqual(result, 1)
+
+        result = subrectangle_queries.get_value(1,0)
+        self.assertEqual(result, 4)
+
+        result = subrectangle_queries.get_value(3,1)
+        self.assertEqual(result, 1)
+
+        subrectangle_queries.update_subrectangle_2(0,0,1,2,100)
+        result = subrectangle_queries.get_value(0,1)
+        self.assertEqual(result, 100)
+
+        subrectangle_queries.update_subrectangle_2(2,0,2,2,90)
+        result = subrectangle_queries.get_value(2,1)
+        self.assertEqual(result, 90)
+
+        subrectangle_queries.update_subrectangle_2(3,0,3,2,80)
+        result = subrectangle_queries.get_value(3,1)
+        self.assertEqual(result, 80)
+
+        subrectangle_queries = SubrectangleQueries([[6,9,6,1,2],[8,8,6,5,9],[7,6,10,8,2],[7,7,4,9,1]])
+        subrectangle_queries.update_subrectangle_2(1,4,2,4,5)
+        result = subrectangle_queries.get_value(3,4)
+        self.assertEqual(result, 1)
+
+        subrectangle_queries.update_subrectangle_2(3,4,3,4,8)
+        result = subrectangle_queries.get_value(2,0)
+        self.assertEqual(result, 7)
+
+
+    
+    def test_group_the_people(self):
+        group_sizes = [3,3,3,3,3,1,3]
+        result = group_the_people(group_sizes)
+        self.assertEqual(result, [[0,1,2],[3,4,6],[5]])
+
+        group_sizes = [2,1,3,3,3,2]
+        result = group_the_people(group_sizes)
+        self.assertEqual(result, [[0,5],[1],[2,3,4]])
+
+    
+    def test_max_increase_keeping_skyline(self):
+        grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+        result = max_increase_keeping_skyline(grid)
+        self.assertEqual(result, 35)
+
+        grid = [[5,1,4],[0,2,3],[7,1,9]]
+        result = max_increase_keeping_skyline(grid)
+        self.assertEqual(result, 6)
+
+        grid = [[1,4,2,7,9],[8,3,5,7,4],[5,9,2,3,2],[3,8,1,5,1],[6,9,2,9,0]]
+        result = max_increase_keeping_skyline(grid)
+        self.assertEqual(result, 79)
 
 
 
