3c8d6eaae0031c6710fe916071bb456bf710b2cc
apulps <apinedaminon@gmail.com>
2020-10-21 16:32:50 +0200

    Add TreeNode class

--- easy_problems/sorted_array_to_BTS.py
+++ easy_problems/sorted_array_to_BTS.py
@@ -3,7 +3,13 @@ Given an array where elements are sorted in ascending order, convert it to a hei
 
 For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
 """
-from assets.problems_data_structures import TreeNode 
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 def sorted_array_to_BTS(nums):
